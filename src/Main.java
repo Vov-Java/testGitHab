ff743bed63195c28636539fa21d53cad710043d7
vov-java <wolf412@bk.ru>
2025-04-03 10:59:38 +0300

    HM_Module3_(class)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        Arithmetic myAry = new Arithmetic();
+        System.out.println("сумма чисел = " + myAry.sumNumbers(3,5));
+        System.out.println("минимальное число =  " + myAry.minNumber(5,2));
     }
 }
+
